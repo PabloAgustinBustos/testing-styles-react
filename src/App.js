2470944544a6819bf986d250a81a42bb5a77ef37
Pablo <pablobustos609@gmail.com>
2022-09-18 04:17:39 -0300

    classic style

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Classic from './components/Classic';
+
+function App() {
+  return (
+    <div className="App">
+      <Classic/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Classic.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/classic.css"
+
+const Classic = () => {
+    return (
+        <div className='classic-container'>
+            Classic
+        </div>
+    )
+}
+
+export default Classic
